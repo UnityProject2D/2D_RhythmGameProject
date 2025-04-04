9824a58bae1d2217911e4b5395e344744e86bbca
kinggodmini <nam3856@gmail.com>
2025-04-04 23:30:30 +0900

    feat: 상점 시스템 구현
    - ShopSystem.cs: 데이터에서 랜덤으로 뽑아서 장비, 소비 아이템 따로 상점에 출력합니다.
    
    - 기획: 장비 아이템 2개, 소비 아이템 4개 상점에 랜덤으로 표시
    - 테스트: ShopTest Scene에서 버튼들 클릭해보기

---
+++ Assets/02.Scripts/Shop/ShopSystem.cs
@@ -0,0 +1,86 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class ShopSystem : MonoBehaviour
+{
+    [Header("상점 슬롯")]
+    [SerializeField] private Transform _equipSlotParent;
+    [SerializeField] private Transform _consumableSlotParent;
+    [SerializeField] private GameObject _shopSlotPrefab;
+
+    [Header("판매 상품 데이터")]
+    [SerializeField] private List<ShopItemSO> _equipmentItems;
+    [SerializeField] private List<ShopItemSO> _consumableItems;
+
+    [Header("아이템 갯수")]
+    [SerializeField]
+    [Tooltip("장비 아이템")]
+    private int _equipmentItemsCount;
+    [SerializeField]
+    [Tooltip("소비 아이템")]
+    private int _consumableItemsCount;
+
+    private List<ShopItemSO> _currentEquipmentItems = new();
+    private List<ShopItemSO> _currentConsumableItems = new();
+
+
+    private void Start()
+    {
+        GenerateShopItems();
+    }
+
+    public void GenerateShopItems()
+    {
+        _currentEquipmentItems.Clear();
+        _currentConsumableItems.Clear();
+
+        var equipment = GetRandomItems(_equipmentItems, _equipmentItemsCount);
+        _currentEquipmentItems.AddRange(equipment);
+
+        var consumables = GetRandomItems(_consumableItems, _consumableItemsCount);
+        _currentConsumableItems.AddRange(consumables);
+
+        CreateSlots();
+    }
+
+    private void CreateSlots()
+    {
+        foreach (Transform child in _equipSlotParent)
+        {
+            Destroy(child.gameObject);
+        }
+        foreach (Transform child in _consumableSlotParent)
+        {
+            Destroy(child.gameObject);
+        }
+
+
+        foreach (var item in _currentEquipmentItems)
+        {
+            var slot = Instantiate(_shopSlotPrefab, _equipSlotParent);
+            var slotUI = slot.GetComponent<ShopSlotUI>();
+            slotUI.Setup(item);
+        }
+        foreach (var item in _currentConsumableItems)
+        {
+            var slot = Instantiate(_shopSlotPrefab, _consumableSlotParent);
+            var slotUI = slot.GetComponent<ShopSlotUI>();
+            slotUI.Setup(item);
+        }
+    }
+
+    private List<ShopItemSO> GetRandomItems(List<ShopItemSO> sourceList, int count)
+    {
+        var copy = new List<ShopItemSO>(sourceList);
+        var result = new List<ShopItemSO>();
+
+        for (int i = 0; i < count && copy.Count > 0; i++)
+        {
+            int index = Random.Range(0, copy.Count);
+            result.Add(copy[index]);
+            copy.RemoveAt(index);
+        }
+
+        return result;
+    }
+}
