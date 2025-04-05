8b700c51a8edfab35c4f710210cf66bb09c65a3f
kinggodmini <nam3856@gmail.com>
2025-04-06 02:17:03 +0900

    feat: 재화 시스템 추가
    - CurrencyManager 구현
    - QuantumKey: (추후)메타 업그레이드 / 장비용 재화로 기획
    - Credit: 소비 아이템용 재화로 기획

---
+++ Assets/02.Scripts/Shop/CurrencyManager.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.Events;
+using System.Collections.Generic;
+
+public enum CurrencyType { QuantumKey, Credit }
+
+public class CurrencyManager : MonoBehaviour
+{
+    public static CurrencyManager Instance { get; private set; }
+
+    private Dictionary<CurrencyType, int> currencies = new();
+
+    public UnityEvent<CurrencyType, int> OnCurrencyChanged;
+
+    void Awake()
+    {
+        if (Instance == null) Instance = this;
+        else Destroy(gameObject);
+
+        foreach (CurrencyType type in System.Enum.GetValues(typeof(CurrencyType)))
+        {
+            currencies[type] = 0;
+        }
+    }
+
+    public int Get(CurrencyType type) => currencies[type];
+
+    public bool CanAfford(CurrencyType type, int amount) => currencies[type] >= amount;
+
+    public bool TrySpend(CurrencyType type, int amount)
+    {
+        if (CanAfford(type, amount))
+        {
+            currencies[type] -= amount;
+            OnCurrencyChanged?.Invoke(type, currencies[type]);
+            return true;
+        }
+        return false;
+    }
+
+    public void Add(CurrencyType type, int amount)
+    {
+        currencies[type] += amount;
+        OnCurrencyChanged?.Invoke(type, currencies[type]);
+    }
+}
