f35cfdff884e772ca121a6b2ade7b7e37a8b9efa
Hjj0731 <84975902+HongMyengJin@users.noreply.github.com>
2025-04-07 12:45:27 +0900

    feat(map): ScriptableObject 기반 방향별 허용 이웃 타일 배열 구조 추가
    - 타일 연결 규칙을 ScriptableObject로 분리하여 데이터화
    - 각 타일별로 4방향(N, E, S, W)에 허용 가능한 이웃 타일을 Constraints 클래스(방향, 리스트)로 저장

---
+++ Assets/02.Scripts/Room/Data/TileData/TileData.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "TileData", menuName = "Scriptable Objects/TileData")]
+public class TileData : ScriptableObject
+{
+    public int Id;
+    public Sprite Sprite;
+
+    [Serializable]
+    public class Constraints
+    {
+        public WaveFunction.DIRECT Direction; // 방향
+        public List<int> AllowNeighbours; // 허용된 이웃 타일 리스트
+    }
+
+    public List<Constraints> constraints = new List<Constraints>();
+}
