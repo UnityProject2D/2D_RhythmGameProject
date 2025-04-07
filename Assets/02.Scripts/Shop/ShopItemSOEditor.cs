72b63558295eae9d9eb45ea1031242e535f1b52b
Jeongyeon Park <ririsid@users.noreply.github.com>
2025-04-07 17:22:25 +0900

    아이템 6종 추가

--- Assets/02.Scripts/Shop/ShopItemSOEditor.cs
+++ Assets/02.Scripts/Shop/ShopItemSOEditor.cs
@@ -18,6 +18,44 @@ public class ShopItemSOEditor : Editor
         item.currencyType = (CurrencyType)EditorGUILayout.EnumPopup("재화 종류", item.currencyType);
         item.category = (ItemCategorySO)EditorGUILayout.ObjectField("카테고리", item.category, typeof(ItemCategorySO), false);
 
+        EditorGUILayout.Space();
+        EditorGUILayout.LabelField("=== 효과 목록 ===", EditorStyles.boldLabel);
+
+        SerializedProperty effectsProperty = serializedObject.FindProperty("effects");
+        EditorGUI.BeginChangeCheck();
+
+        // Draw the size field
+        EditorGUILayout.PropertyField(effectsProperty.FindPropertyRelative("Array.size"), new GUIContent("효과 개수"));
+
+        // Draw each element
+        for (int i = 0; i < effectsProperty.arraySize; i++)
+        {
+            SerializedProperty effectProperty = effectsProperty.GetArrayElementAtIndex(i);
+            EditorGUILayout.BeginVertical(GUI.skin.box);
+            EditorGUILayout.LabelField($"효과 {i + 1}", EditorStyles.boldLabel);
+
+            EditorGUILayout.PropertyField(effectProperty.FindPropertyRelative("effectType"), new GUIContent("효과 타입"));
+            EditorGUILayout.PropertyField(effectProperty.FindPropertyRelative("value"), new GUIContent("값"));
+
+            if (GUILayout.Button("삭제"))
+            {
+                effectsProperty.DeleteArrayElementAtIndex(i);
+                break;
+            }
+            EditorGUILayout.EndVertical();
+            EditorGUILayout.Space();
+        }
+
+        if (GUILayout.Button("효과 추가"))
+        {
+            effectsProperty.arraySize++;
+        }
+
+        if (EditorGUI.EndChangeCheck())
+        {
+            serializedObject.ApplyModifiedProperties();
+        }
+
 
         EditorGUILayout.Space();
         EditorGUILayout.LabelField("=== 구매 시 효과 이벤트 ===", EditorStyles.boldLabel);
