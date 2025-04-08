99385335c9d65088edccb9ccc26b03b2c0967eae
Hjj0731 <84975902+HongMyengJin@users.noreply.github.com>
2025-04-08 22:01:22 +0900

    feat(lighting): 상점 조명 MMF Player Settings 설정 완료
    - 상점 씬 조명을 위한 MMF Player Settings 내 조명 관련 파라미터 설정
    - 조명이 깜빡이는 효과를 구현

--- Assets/02.Scripts/Room/LightIntensityFader.cs
+++ Assets/02.Scripts/Room/LightIntensityFader.cs
@@ -1,48 +1,50 @@
+using MoreMountains.Feedbacks;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
 
-public class LightIntensityFader : MonoBehaviour
-{
-    [Header(" Light 설정")]
-    public Light2D TargetLight;
+public class LightIntensityFader : MonoBehaviour {
+    //{
+    //    [Header(" Light 설정")]
+    //    public Light2D TargetLight;
 
-    [Header(" Light Intensity 설정")]
-    public float minIntensity = 0.2f;
-    public float maxIntensity = 0.2f;
-    public float duration = 1.0f;
+    //    [Header(" Light Intensity 설정")]
+    //    public float minIntensity = 0.2f;
+    //    public float maxIntensity = 0.2f;
+    //    public float duration = 1.0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
     // Update is called once per frame
 
-
+    public MMF_Player MMF_Player;
     private void Start()
     {
-        StartCoroutine(FadeLoop());
+        MMF_Player?.PlayFeedbacks();
+        //StartCoroutine(FadeLoop());
     }
 
 
-    private IEnumerator FadeLoop()
-    {
-        while(true)
-        {
-            // 밝아지기
-            yield return StartCoroutine(FadeIntensity(minIntensity, maxIntensity, duration));
-            // 어두워지기
-            yield return StartCoroutine(FadeIntensity(maxIntensity, minIntensity, duration));
-        }
-    }
-    private IEnumerator FadeIntensity(float start, float end, float time)
-    {
-        float elapsed = 0.0f;
-        while (elapsed < time)
-        {
-            float t = elapsed / time;
-            TargetLight.intensity = Mathf.Lerp(start, end, t);
-            elapsed += Time.deltaTime;
-            yield return null;
-        }
-        TargetLight.intensity = end;
-    }
+    //private IEnumerator FadeLoop()
+    //{
+    //    while(true)
+    //    {
+    //        // 밝아지기
+    //        yield return StartCoroutine(FadeIntensity(minIntensity, maxIntensity, duration));
+    //        // 어두워지기
+    //        yield return StartCoroutine(FadeIntensity(maxIntensity, minIntensity, duration));
+    //    }
+    //}
+    //private IEnumerator FadeIntensity(float start, float end, float time)
+    //{
+    //    float elapsed = 0.0f;
+    //    while (elapsed < time)
+    //    {
+    //        float t = elapsed / time;
+    //        TargetLight.intensity = Mathf.Lerp(start, end, t);
+    //        elapsed += Time.deltaTime;
+    //        yield return null;
+    //    }
+    //    TargetLight.intensity = end;
+    //}
 }
 
