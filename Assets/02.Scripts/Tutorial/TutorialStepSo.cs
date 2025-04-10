468601e2bd6ee0d041b3dd77bcb72a089f54752f
Hjj0731 <84975902+HongMyengJin@users.noreply.github.com>
2025-04-10 18:40:57 +0900

    feat(tutorial): SequenceSO 기반 튜토리얼 재생 로직 구현 및 MMF 이벤트 연동
    - 튜토리얼 매니저에서 SequenceSO를 받아 StepSO 순차 출력 기능 구현
    - 각 StepSO의 텍스트, 출력 속도, 딜레이 시간에 따라 출력되도록 구성
    - MMFPlayer Complete 이벤트에 연결하여 다음 StepSO 자동 출력 처리
    - 튜토리얼 흐름 전체가 ScriptableObject 기반으로 재생되도록 연동 완료

--- Assets/02.Scripts/Tutorial/TutorialStepSo.cs
+++ Assets/02.Scripts/Tutorial/TutorialStepSo.cs
@@ -1,10 +1,14 @@
+using MoreMountains.Feedbacks;
 using UnityEngine;
 using UnityEngine.Events;
+using TMPro;
+using Unity.VisualScripting;
+using System;
 
 [CreateAssetMenu(fileName = "TutorialStepSo", menuName = "Scriptable Objects/TutorialStepSo")]
 public class TutorialStepSo : ScriptableObject
 {
     [TextArea] public string Text;
-    public UnityEvent OnStartEvent;
-    public UnityEvent OnEndEvent;
+    public float Duration;
+    public float DelayTime;
 }
