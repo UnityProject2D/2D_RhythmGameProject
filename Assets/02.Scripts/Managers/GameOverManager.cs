3ec24750ad28816d0cdfa1f8855aa545f48c210f
nam3856 <nam3856@gmail.com>
2025-04-11 16:56:34 +0900

    feat(GameOverManager): 게임 오버 구현
    - 죽는 상황이 된다면:
    - 음악이 느려짐
    - n초 후 게임오버 씬으로 이동

---
+++ Assets/02.Scripts/Managers/GameOverManager.cs
@@ -0,0 +1,51 @@
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using System.Collections;
+using FMODUnity;
+using FMOD.Studio;
+
+public class GameOverManager : MonoBehaviour
+{
+    public static GameOverManager Instance { get; private set; }
+    public int WaitTime = 4; // 대기 시간 (초)
+    public int WaitTime2 = 1; // 대기 시간 (초)
+
+    public float startPitch = 1f;
+    public float endPitch = 0.2f;
+    [SerializeField] private string gameOverSceneName = "UI_GameOver";
+    private Bus masterBus;
+
+    private void Awake()
+    {
+        masterBus = RuntimeManager.GetBus("bus:/");
+        if (Instance == null) Instance = this;
+        else Destroy(gameObject);
+    }
+    private void Start()
+    {
+        GameManager.Instance.PlayerRegistered += () =>
+        {
+            Debug.Log("[RhythmManager] PlayerRegistered");
+            GameManager.Instance.Player.Health.OnPlayerDied += OnPlayerDied;
+        };
+    }
+
+    public void OnPlayerDied()
+    {
+        Debug.Log("[GameOverManager] 게임 오버 처리");
+        StartCoroutine(HandleDie());
+    }
+
+    private IEnumerator HandleDie()
+    {
+        Debug.Log("[GameOverManager] HandleDie() 호출");
+        Time.timeScale = 0.2f;
+
+        yield return new WaitForSecondsRealtime(WaitTime);
+        Time.timeScale = 1f;
+        yield return new WaitForSecondsRealtime(WaitTime2);
+        SceneManager.LoadScene(gameOverSceneName);
+    }
+
+    
+}
